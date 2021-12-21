256278d8008f03361a331a1e9b26ce735c760604
Alexandr Chigrinets <alexandr@wps.su>
2021-12-21 13:01:35 +0300

    Update Applicative trait

--- monad/src/main/scala/net/iponweb/monad/Applicative.scala
+++ monad/src/main/scala/net/iponweb/monad/Applicative.scala
@@ -10,9 +10,12 @@ trait Applicative[F[_]] extends Functor[F] {
   def map[A, B](fa: F[A])(f: A => B): F[B] =
     ap(pure(f))(fa)
 
+  def product[A, B](fa: F[A], fb: F[B]): F[(A, B)] =
+    ap(map(fa)(a => (b: B) => (a, b)))(fb)
+
   def map2[A, B, C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C] =
-    ap(map(fb)(b => (a: A) => f(a, b)))(fa)
+    map(product(fa, fb))(f.tupled)
 
   def map3[A, B, C, D](fa: F[A], fb: F[B], fc: F[C])(f: (A, B, C) => D): F[D] =
-    ap(map2(fb, fc)((b, c) => (a: A) => f(a, b, c)))(fa)
+    map2(fa, product(fb, fc)) { case (a, (b, c)) => f(a, b, c) }
 }
